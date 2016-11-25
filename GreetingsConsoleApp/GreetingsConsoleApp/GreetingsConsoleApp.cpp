55f9fe64c74b3ccc95eb6e17a0d6b2ec1dcbdfbc
mario <marioljof@outlook.com>
2016-11-25 18:58:44 -0500

    First app.

---
+++ GreetingsConsoleApp/GreetingsConsoleApp/GreetingsConsoleApp.cpp
@@ -0,0 +1,12 @@
+// GreetingsConsoleApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>  
+using namespace std;
+
+int main()
+{
+	cout << "Hello" << endl;
+    return 0;
+}
