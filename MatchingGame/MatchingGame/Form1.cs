b63abe0512b7a1d5331d9377abf6a2486b213ee9
mario <marioljof@outlook.com>
2016-12-02 21:55:34 -0500

    Delete empty line.

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -142,7 +142,6 @@ namespace MatchingGame
                 {
                     firstClicked = clickedLabel;
                     firstClicked.ForeColor = Color.Black;
-
                     return;
                 }
 
